65cb335e2f93b8b2e0faf9156e0cccc7f63d73ab
Antônio Lopes <antoniolopesg@protonmail.com>
2019-08-14 02:01:25 -0300

    creating field password for user and install bcrypt

--- common/env.ts
+++ common/env.ts
@@ -4,5 +4,8 @@ export const environment = {
     },
     db: {
         uri: process.env. DB_URI || 'mongodb://localhost/peaky-blinders'
+    },
+    security: {
+        saltRounds: process.env.SALT_ROUNDS || 10
     }
 }
--- users/users.model.ts
+++ users/users.model.ts
@@ -1,14 +1,31 @@
 import * as mongoose from 'mongoose'
+import * as bcrypt from 'bcrypt'
+import { environment } from '../common/env';
 
 export interface User extends mongoose.Document {
     email: string,
-    name: string
+    name: string,
+    password: string
 }
 
 const userSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String, unique: true },
-    password: { type: String }
+    name: { type: String , required: true},
+    email: { type: String, unique: true , required: true},
+    password: { type: String , required: true}
+});
+
+userSchema.pre('save', function(next) {
+    const user: User = this;
+
+    if(!user.isModified('password')){
+        next();
+    } else {
+        bcrypt.hash(user.password, environment.security.saltRounds).then(hash => {
+            user.password = hash;
+            next();
+        }).catch(next);
+    }
+
 })
 
 export const User = mongoose.model<User>('User', userSchema, 'pb');
