ec39619395710fb117efc0bc3d62d954b03577b6
Antônio Lopes <antoniolopesg@protonmail.com>
2019-08-15 01:46:22 -0300

    Adding a Routing Mondel to Avoid Repeated Codes

---
+++ common/model-router.ts
@@ -0,0 +1,63 @@
+import { Router } from './router';
+import * as mongoose from 'mongoose';
+
+export abstract class ModelRouter<D extends mongoose.Document> extends Router {
+    constructor(protected model: mongoose.Model<D>){
+        super();
+    }
+
+    validateId  = (req, res, next) => {
+        if(mongoose.Types.ObjectId.isValid(req.params.id)){
+            res.send(404);
+            next(new Error('Not found error'));
+        } else {
+            next();
+        }
+    }
+
+    findAll = (req, res, next) => {
+        this.model.find()
+        .then(this.renderAll(res, next))
+        .catch(next)
+    }
+
+    findById = (req, res, next) => {
+        this.model.findById(req.params.id)
+        .then(this.render(res, next))
+        .catch(next);
+    }
+
+    save = (req, res, next) => {     
+        let document = new this.model(req.body);
+        document.save().then(this.render(res, next)).catch(next);
+    }
+
+    replace = (req, res, next) => {
+        const options = {runValidators: true, overwrite: true}
+        this.model.update({_id: req.params.id}, req.body, options)
+            .exec().then(result => {
+                if(result.n){
+                    return this.model.findById(req.params.id)
+                } else {
+                    res.send(404, {err: 'not found'});
+                }
+        }).then(this.render(res, next)).catch(next);
+    }
+
+    update = (req, res,next) => {
+        const options = {runValidators: true, new: true}
+        this.model.findOneAndUpdate(req.params.id, req.body, options)
+            .then(this.render(res, next)).catch(next);
+    }
+
+    delete = (req, res, next) => {
+        this.model.remove({_id: req.params.id}).exec().then((res: any) => {
+            if(res.result.n){
+                res.send(204);
+            } else {
+                res.send(404);
+            }
+            return next();
+        }).catch(next);
+    }
+}
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,67 +1,27 @@
-import { Router } from '../common/router';
+import { ModelRouter } from '../common/model-router';
 import * as restify from 'restify';
 import { User } from './users.model'
 
-class UsersRouter  extends Router {
+class UsersRouter extends ModelRouter<User> {
     constructor() {
-        super();
+        super(User);
         this.on('beforeRender', document => {
             document.password = undefined;
         });
     }
 
     applyRoutes(application: restify.Server){
-        application.get('/users', (req, res, next) => {
-            User.find()
-            .then(this.render(res, next))
-            .catch(next)
-        });
+        application.get('/users', this.findAll);
 
-        application.get('/users/:id', (req, res, next) => {
-            User.findById(req.params.id).then(this.render(res, next)).catch(next);
-        });
+        application.get('/users/:id', [this.validateId, this.findById]);
 
-        application.post('/users', (req, res, next) => {
-            const { name, email } = req.body;
+        application.post('/users', this.save);
 
-            if(!name || !email){
-                res.json({err: 'email and password fields are required'});
-                return next();
-            }
-            
-            let user = new User(req.body);
-            user.save().then(this.render(res, next)).catch(next);
+        application.put('/users/:id', [this.validateId, this.replace]);
 
-        });
+        application.patch('/users/:id', [this.validateId, this.update]);
 
-        application.put('/users/:id', (req, res, next) => {
-            const options = {runValidators: true, overwrite: true}
-            User.update({_id: req.params.id}, req.body, options)
-                .exec().then((result: any) => {
-                    if(result.n){
-                        return User.findById(req.params.id)
-                    } else {
-                        res.send(404, {err: 'not found'});
-                    }
-            }).then(this.render(res, next)).catch(next);
-        })
-
-        application.patch('/users/:id', (req, res,next) => {
-            const options = {runValidators: true, new: true}
-            User.findOneAndUpdate(req.params.id, req.body, options)
-                .then(this.render(res, next)).catch(next);
-        });
-
-        application.del('/users/:id', (req, res, next) => {
-            User.remove({_id: req.params.id}).exec().then((res: any) => {
-                if(res.result.n){
-                    res.send(204);
-                } else {
-                    res.send(404);
-                }
-                return next();
-            }).catch(next);
-        });
+        application.del('/users/:id', [this.validateId, this.delete]);
     }
 }
 
